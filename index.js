d36016de7f03a5ba30b588aad066ef25cd89df38
Berk Altıok <berkpw@gmail.com>
2021-10-21 20:27:22 +0300

    feat: create example data

---
+++ index.js
@@ -0,0 +1,8 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running\nhttp://localhost:3000')
+})
