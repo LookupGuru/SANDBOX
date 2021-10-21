6cefaeeb875948cf9fb9d9ec3beae423874dd779
Berk Altıok <berkpw@gmail.com>
2021-10-21 20:57:38 +0300

    chore: update server package

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const express = require('express')
+const consola = require('consola')
+const database = require("./db.json");
 
-server.use(router)
-server.listen(3600, () => {
-  console.log('SANDBOX Server is running\nhttp://localhost:3600')
+const app = express();
+
+app.use(require("cors")({
+  origin: "http://localhost:8080",
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
+app.all("/lookup", (req, res) => {
+  res.json(database.lookup);
 })
+app.listen(3600, () => consola.success("Server listening on http://localhost:3600"));
